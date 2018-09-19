49fb44508a7b3fc9953b3a3c9ede9746daf463bf
kor1999 <kor1999@mail.ru>
2018-09-19 22:40:35 +0300

    add ex2

---
+++ Week5/ex2.sh
@@ -0,0 +1,9 @@
+                                                                                             
+i=2000
+while [ $i -gt 0 ]
+do
+a=`tail -n 1 ex2.txt`
+a=$((a + 1))
+echo "$a" >> ex2.txt
+i=$((i - 1))
+done
