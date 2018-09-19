6275de1c62250b7d6cfb7441844a6b880aeffaa1
kor1999 <kor1999@mail.ru>
2018-09-19 19:01:21 +0300

    Added ex1

---
+++ Week5/ex1(afterFix).c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include <pthread.h>
+
+void *myThreadFun(void* i)
+{
+    printf("Some text from thread: %d\n", (int) i);
+
+    return NULL;
+}
+
+int main() {
+    int n = 10;
+    pthread_t thread;
+    for (int i = 0; i < n; i++) {
+        pthread_create(&thread, NULL, myThreadFun, (void *) i);
+        pthread_join(thread, NULL);
+    }
+    
+    //pthread_join(thread, NULL);
+    return 0;
+}
